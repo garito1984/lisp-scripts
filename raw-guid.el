755240a4cb47647242568e24712f3c664d623da2
Your Name <you@example.com>
2023-09-24 17:26:42 +0200

    Add Oracle raw-to-uuid command

---
+++ raw-guid.el
@@ -0,0 +1,42 @@
+;;
+;; Convert Oracle raw values into UUID and back
+;;
+;; UUID: 550e8400-e29b-41d4-a716-446655440000
+;; RAW:      550E8400E29B41D4A716446655440000
+;;
+
+(defun fa-guid-convert-region ()
+  "Convert value from raw to string and vice versa"
+  (interactive)
+  (let* ((str (buffer-substring-no-properties (region-beginning) (region-end)))
+	 (str-len (length str))
+	 (converted-str str))
+    (cond ((equal str-len 36) ; UUID
+	   (setq converted-str (fa--guid-to-raw str)))
+	  ((equal str-len 32) ; RAW
+	   (setq converted-str (fa--raw-to-guid str)))
+	  (t
+	   (error "Not a valid UUID")))
+    (delete-region (region-beginning) (region-end))
+    (insert converted-str)))
+
+(defun fa--guid-to-raw (guid)
+  "Convert guid into raw Oracle string"
+  (string-replace "-" "" (upcase guid)))
+
+(defun fa--raw-to-guid (raw-guid)
+  "Convert raw into a UUID string"
+  (let* ((uuid-first  (substring raw-guid 0 8))
+ 	 (uuid-second (substring raw-guid 8 12))
+	 (uuid-third  (substring raw-guid 12 16))
+	 (uuid-fourth (substring raw-guid 16 20))
+	 (uuid-fifth  (substring raw-guid 20 32)))
+    (downcase
+     (string-join
+      (list uuid-first
+	    uuid-second
+	    uuid-third
+	    uuid-fourth
+	    uuid-fifth)
+      "-"))))
+
