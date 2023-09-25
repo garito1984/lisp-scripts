a40e1122ae4c9057930e5b34b5b27774362fdb94
Your Name <you@example.com>
2023-09-25 20:28:49 +0200

    Add .emacs config file

---
+++ emacs.el
@@ -0,0 +1,49 @@
+(require 'package)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(column-number-mode t)
+ '(custom-enabled-themes '(modus-vivendi))
+ '(electric-pair-mode t)
+ '(global-display-line-numbers-mode t)
+ '(inhibit-startup-screen t)
+ '(menu-bar-mode nil)
+ '(package-archives
+   '(("gnu" . "https://elpa.gnu.org/packages/")
+     ("melpa" . "https://melpa.org/packages/")))
+ '(package-selected-packages '(http rust-mode markdown-mode haskell-mode))
+ '(safe-local-variable-values
+   '((vc-prepare-patches-separately)
+     (diff-add-log-use-relative-names . t)
+     (vc-git-annotate-switches . "-w")))
+ '(tool-bar-mode nil))
+(package-initialize)
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(put 'upcase-region 'disabled nil)
+(put 'downcase-region 'disabled nil)
+
+(defun fa-scroll (lines)
+  "Scroll text of selected window up or down LINES.
+
+Scroll down if LINES is a negative number, scroll up otherwise."
+  (interactive (let ((num (string-to-number (read-buffer "How many to scroll? "))))
+                 (list num)))
+  (if (< lines 0)
+      (scroll-down (* -1 lines)) (scroll-up lines)))
+
+(defun base64url-decode-region ()
+  "FA function in .emacs to decode using base64url instead of the more standard base64 algo.
+
+This is useful for instance to decode JWT tokens"
+  (interactive)
+  (base64-decode-region (region-beginning) (region-end) t))
+
+;; Load personal scripts
+(load-file "~/Documents/lisp-scripts/raw-guid.elc")
