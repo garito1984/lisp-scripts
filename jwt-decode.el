ccf6b48f5d8701d2e1b803a037b72bdc97bfcefe
Your Name <you@example.com>
2023-09-29 00:28:33 +0200

    Add comment

--- jwt-decode.el
+++ jwt-decode.el
@@ -4,6 +4,7 @@
 ;; jwt-token: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c
 
 (defun fa-jwt-decode-region (&optional all)
+  "Decode jwt-token"
   (interactive)
   (let* ((token (buffer-substring-no-properties (region-beginning) (region-end)))
 	 (tokens (split-string token "\\."))
