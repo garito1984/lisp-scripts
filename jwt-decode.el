375873554709c7ddfe633164164d85400c8e1160
Your Name <you@example.com>
2023-10-21 22:30:04 +0200

    Search for the jwt token automatically

--- jwt-decode.el
+++ jwt-decode.el
@@ -3,15 +3,24 @@
 ;;
 ;; jwt-token: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c
 
-(defun fa-jwt-decode-region (&optional all)
+(defun fa-jwt-decode ()
   "Decode jwt-token"
   (interactive)
-  (let* ((token (buffer-substring-no-properties (region-beginning) (region-end)))
+  (fa--jwt-decode-impl (re-search-backward "[^[:graph:]]") (re-search-forward "[[:graph:]]+")))
+
+(defun fa-jwt-decode-region (&optional all)
+  "Decode jwt-token region"
+  (interactive)
+  (fa--jwt-decode-impl (region-beginning) (region-end) all))
+
+(defun fa--jwt-decode-impl (beginning end &optional all)
+  "Decode jwt-token impl"
+  (let* ((token (buffer-substring-no-properties beginning end))
 	 (tokens (split-string token "\\."))
 	 (header (car tokens))
 	 (payload (cadr tokens))
 	 (signature (caddr tokens)))
-    (delete-region (region-beginning) (region-end))
+    (delete-region beginning end)
     (insert
      (string-join
       (list
@@ -21,3 +30,5 @@
 	   (base64-decode-string signature t)
 	 signature))
       "."))))
+
+
