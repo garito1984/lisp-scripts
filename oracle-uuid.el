ee94b033d38f164c2139213c38c9178721dff356
fer <fernandoatrio@gmail.com>
2024-04-14 18:46:32 +0200

    Update function names

--- oracle-uuid.el
+++ oracle-uuid.el
@@ -9,32 +9,32 @@
 (defun fa-oracle-uuid-convert ()
   "Convert raw/uuid value"
   (interactive)
-  (fa--uuid-convert-region-impl (fa--find-uuid-start) (fa--find-uuid-end)))
+  (oracle--uuid-convert-region-impl (oracle--find-uuid-start) (oracle--find-uuid-end)))
 
 (defun fa-oracle-uuid-convert-region ()
   "Convert value from RAW to UUID and vice versa"
   (interactive)
-  (fa--uuid-convert-region-impl (region-beginning) (region-end)))
+  (oracle--uuid-convert-region-impl (region-beginning) (region-end)))
 
-(defun fa--uuid-convert-region-impl (beginning end)
+(defun oracle--uuid-convert-region-impl (beginning end)
   "Convert value from RAW to UUID and vice versa"
   (let* ((str (buffer-substring-no-properties beginning end))
 	 (str-len (length str))
 	 (converted-str str))
     (cond ((equal str-len 36) ; UUID
-	   (setq converted-str (fa--uuid-to-raw str)))
+	   (setq converted-str (oracle--uuid-to-raw str)))
 	  ((equal str-len 32) ; RAW
-	   (setq converted-str (fa--raw-to-uuid str)))
+	   (setq converted-str (oracle--raw-to-uuid str)))
 	  (t
 	   (error "Not a valid RAW or plain UUID")))
     (delete-region beginning end)
     (insert converted-str)))
 
-(defun fa--uuid-to-raw (uuid)
+(defun oracle--uuid-to-raw (uuid)
   "Convert UUID into RAW"
   (upcase (string-replace "-" "" uuid)))
 
-(defun fa--raw-to-uuid (raw-uuid)
+(defun oracle--raw-to-uuid (raw-uuid)
   "Convert RAW into a UUID"
   (let* ((uuid-first  (substring raw-uuid 0 8))
  	 (uuid-second (substring raw-uuid 8 12))
@@ -44,8 +44,8 @@
     (downcase (format "%s-%s-%s-%s-%s"
 	    uuid-first uuid-second uuid-third uuid-fourth uuid-fifth))))
 
-(defun fa--find-uuid-start ()
+(defun oracle--find-uuid-start ()
   (+ (point) (skip-chars-backward "[[:alnum:]-]"))) 
 
-(defun fa--find-uuid-end ()
+(defun oracle--find-uuid-end ()
   (+ (point) (skip-chars-forward "[[:alnum:]-]")))
