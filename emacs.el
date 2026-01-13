e26c955d885f02d504991b2facf3371c7b782d45
fer <fernandoatrio@gmail.com>
2026-01-13 03:30:22 +0100

    Add current line highlighting

--- emacs.el
+++ emacs.el
@@ -8,6 +8,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(column-number-mode t)
+ '(global-hl-line-mode t)
  '(custom-enabled-themes '(modus-vivendi))
  '(electric-pair-mode t)
  '(global-display-line-numbers-mode t)
